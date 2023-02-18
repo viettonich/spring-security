86abf716957a12c3fba6bb61012de7cada2b01cd
vietnb7 <vietnb7@fpt.com.vn>
2023-02-18 21:25:46 +0700

    Revert "update"
    This reverts commit a069960208dfcc732d4ca6b10186195edf36be79.

--- src/main/java/com/example/app/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/app/configuration/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/api/login").permitAll();
+		http.authorizeRequests().antMatchers("/api/timer/**").permitAll();
 		http.authorizeRequests().antMatchers("/api/**").hasAuthority("Admin");
 
 		http.exceptionHandling().accessDeniedHandler(restAccessDeniedHandler)
